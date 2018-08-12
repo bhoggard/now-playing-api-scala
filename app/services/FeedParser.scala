92373bb492d8d926665358791df31524e3425366
Barry Hoggard <barry@hoggard.org>
2018-08-12 16:17:27 -0400

    refactor parser

--- app/services/FeedParser.scala
+++ app/services/FeedParser.scala
@@ -6,20 +6,29 @@ import play.api.libs.json._
 import play.api.libs.json.Reads._
 import play.api.libs.functional.syntax._
 
-object FeedParser {
+trait FeedParser {
 
-  implicit val q2Reads: Reads[Piece] = (
-    (JsPath \ "current_playlist_item" \ "catalog_entry" \ "title")
-      .read[String] and
-      (JsPath \ "current_playlist_item" \ "catalog_entry" \ "composer" \ "name")
-        .read[String]
-  )(Piece.apply _)
+  implicit val reader: Reads[Piece]
 
-  def translate_q2(json: JsValue): Option[Piece] = json.validate[Piece] match {
+  def validationResult(result: JsResult[Piece]): Option[Piece] = result match {
     case s: JsSuccess[Piece] => Some(s.get)
     case e: JsError => {
       Logger.error("Errors: " + JsError.toJson(e).toString)
       None
     }
   }
+
+  def parse(json: JsValue): Option[Piece] =
+    validationResult(json.validate[Piece])
+}
+
+object Q2Parser extends FeedParser {
+
+  implicit val reader: Reads[Piece] = (
+    (JsPath \ "current_playlist_item" \ "catalog_entry" \ "title")
+      .read[String] and
+      (JsPath \ "current_playlist_item" \ "catalog_entry" \ "composer" \ "name")
+        .read[String]
+  )(Piece.apply _)
+
 }
--- test/services/FeedParserSpec.scala
+++ test/services/FeedParserSpec.scala
@@ -2,14 +2,14 @@ package services
 
 import models.Piece
 import org.scalatestplus.play.PlaySpec
-import play.api.libs.json.{JsObject, Json}
+import play.api.libs.json.Json
 
 class FeedParserSpec extends PlaySpec {
 
-  "FeedParser" should {
+  "Q2Parser" should {
     "parse q2 data" in {
       val json = Json.parse(scala.io.Source.fromFile("test/resources/q2.json").mkString)
-      val pieceOpt = FeedParser.translate_q2(json)
+      val pieceOpt = Q2Parser.parse(json)
       pieceOpt mustEqual Some(Piece(title = "Mask", composer =  "Visible Cloaks"))
     }
   }
